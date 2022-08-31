7d237677ca39577a33ddb2c7b3ef9c9a5539b732
williamqzy <qzhiyua@g.clemson.edu>
2022-08-30 22:27:46 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='mcabc',
+    version='0.0.1',
+    description='Model Comparison in Approximate Bayesian Computation',
+    url='https://github.com/janfb/mcabc',
+    author='janfb',
+    packages=['mcabc', 'mcabc.mdn', 'mcabc.model', 'mcabc.utils', 'tests'],
+    license='MIT',
+    long_description=read('README.md'),
+    install_requires=['numpy', 'scipy', 'tqdm', 'torch', 'matplotlib', 'jupyter']
+)
